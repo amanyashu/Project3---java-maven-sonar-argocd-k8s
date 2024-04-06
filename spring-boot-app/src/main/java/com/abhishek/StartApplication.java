30cbb65ec71587135ca7df5f9ec6857ab79eeb19
Sahana-sonu <93938850+Sahana-sonu@users.noreply.github.com>
2024-04-06 00:27:54 -0700

    Update StartApplication.java

--- spring-boot-app/src/main/java/com/abhishek/StartApplication.java
+++ spring-boot-app/src/main/java/com/abhishek/StartApplication.java
@@ -2,14 +2,20 @@ package com.abhishek;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @Controller
 public class StartApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(StartApplication.class);
+    }
 
     @GetMapping("/")
     public String index(final Model model) {
@@ -21,5 +27,4 @@ public class StartApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(StartApplication.class, args);
     }
-
 }
